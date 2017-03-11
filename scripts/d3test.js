7b514eff95444d8890ba603f8b3669c5691c8152
Chintan Jariwala <chintan.jari1993@gmail.com>
2017-03-10 17:54:56 -0700

    Added weather data functionality

--- scripts/d3test.js
+++ scripts/d3test.js
@@ -9,15 +9,13 @@
   function constructor(){
     d3.request(currentWeatherUrlBuilder("33.38","-111.93")).get(function (response) {
       var res = JSON.parse(response.response);
-      console.log("Current weather data");
-      console.log(res);
+
+      //displayWeatherGraph(res.main);
     });
 
     d3.request(futureWeatherUrlBuilder("33.38","-111.93")).get(function (response) {
       var res = JSON.parse(response.response);
-      console.log("Future weather data");
-
-      console.log(res);
+      displayWeatherGraph(res.list);
     });
 
   }
@@ -27,7 +25,8 @@
     currentWeatherUrl = baseUrl +
                         "data/2.5/weather?" +
                         "lat=" + lat + "&" + "lon=" + long + "&" +
-                        "appid=" + apiid;
+                        "appid=" + apiid + "&" +
+                        "&units=metric";
 
     return currentWeatherUrl;
   }
@@ -36,9 +35,66 @@
     currentWeatherUrl = baseUrl +
                         "data/2.5/forecast?" +
                         "lat=" + lat + "&" + "lon=" + long + "&" + "cnt=15" + "&" +
-                        "appid=" + apiid;
+                        "appid=" + apiid + "&" +
+                        "&units=metric";
 
     return currentWeatherUrl;
+  }
+
+  function displayWeatherGraph(data){
+      // Set the dimensions of the canvas / graph
+      var margin = {top: 30, right: 20, bottom: 30, left: 20},
+          width = 650 - margin.left - margin.right,
+          height = 250 - margin.top - margin.bottom;
+
+      // Set the ranges
+      // var x = d3.time.scale().range([0, width]);
+      // var y = d3.scale.linear().range([height, 0]);
+
+      var svg = d3.select("#weatherDetail").append('svg')
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+      var parseTime = d3.timeParse("%d-%b-%y");
+      console.log(data);
+      var lineData = [],
+          day = {};
+      var date = "";
+      data.forEach(function (d) {
+          //console.log(parseTime(date));
+          day = {date: parseTime(new Date(d.dt_txt)) , temp : +d.main.temp }
+          lineData.push(day);
+      });
+      console.log(lineData[0].date);
+      data = lineData;
+      var x = d3.scaleLinear().range([0, width]);
+      var y = d3.scaleLinear().range([height, 0]);
+
+      // define the line
+      var valueline = d3.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.temp); });
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain([0, d3.max(data, function(d) { return d.temp; })]);
+
+      // Add the value-line path.
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+          .attr("d", valueline);
+
+      // Add the X Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // Add the Y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
   }
 
 
